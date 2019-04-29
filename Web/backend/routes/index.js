7926d46bb4e8ea679a46ce7cbb2c142e15e6de75
karkay <kentkfeng@gmail.com>
2019-04-29 19:43:36 -0400

    fixed the backend with new queries

--- Web/backend/routes/index.js
+++ Web/backend/routes/index.js
@@ -22,10 +22,12 @@ async function executeQuery(queryString){
 router.get('/getUserInfo/:userId',(req,res)=>{
   const userId = req.params.userId;
   console.log("userId: ", userId);
-  const formattedQuery = `SELECT * FROM USER WHERE u_id = ${userId}`;
-  const userInfo = executeQuery(formattedQuery);
-  res.send(userInfo);
-  console.log(`getUserInfo for ${userId}`);
+  const formattedQuery = `SELECT * FROM USER WHERE u_id = "${userId}"`;
+  executeQuery(formattedQuery).then(userInfo=>{
+      console.log(`getUserInfo for ${userId[0]}`);
+      res.send(userInfo[0]);
+  });
+
   //select user info from table
 
 });
@@ -33,45 +35,22 @@ router.get('/getUserInfo/:userId',(req,res)=>{
 //this route returns the users transactions given the user id
 router.get('/getUserTrans/:userId',(req,res)=>{
     const userId = req.params.userId;
-    res.send(`getUserTrans for ${userId}`)
+    
     //select user transactions with joins from tables
-    const formattedQuery = `
-      SELECT UT.first_name, 
-             UT.middle_initial, 
-             UT.last_name, 
-             UT.transaction_date_time,
-             BI.business_name, 
-             BI.item_name, 
-             BI.price 
-      FROM (
-        SELECT U.first_name, 
-               U.middle_initial, 
-               U.last_name, 
-               T.item_id, 
-               T.u_id, 
-               T.b_id, 
-               T.transaction_date_time 
-        FROM user U JOIN transaction T ON U.u_id = T.u_id
-      ) UT
-        JOIN (
-          B.b_id, 
-          B.business_name, 
-          I.item_id, 
-          I.item_name, 
-          I.price
-
-          FROM business B JOIN item I ON B.b_id = I.b_id
-        ) BI
-        ON 
-           UT.item_id = BI.item_id AND 
-           UT.b_id = BI.b_id 
-
-      WHERE u_id = ${userId};
-
-    `;
-    const userTrans = executeQuery(formattedQuery);
-    res.send(userTrans);
-    console.log(`getUserTrans for ${userId}`);
+    const formattedQuery = `SELECT UT.first_name, UT.middle_initial, UT.last_name, UT.transaction_date_time,
+  BI.business_name, BI.item_name, BI.price 
+  FROM (SELECT U.first_name, U.middle_initial, U.last_name, 
+    T.item_id, T.u_id, T.b_id, T.transaction_date_time 
+    FROM user U JOIN transaction T ON U.u_id = T.u_id) UT
+  JOIN (SELECT B.b_id, B.business_name, I.item_id, I.item_name, I.price
+    FROM business B JOIN item I ON B.b_id = I.b_id) BI
+    ON UT.item_id = BI.item_id AND UT.b_id = BI.b_id 
+WHERE u_id = "${userId}";`;
+    executeQuery(formattedQuery).then(userTrans=>{
+      res.send(userTrans[0]);
+      console.log(`getUserTrans for ${userId}`);
+    });
+    
 
 
 });
@@ -79,29 +58,35 @@ router.get('/getUserTrans/:userId',(req,res)=>{
 //this route returns the list of businesses in the business table.
 router.get('/getBusinesses',(req,res)=>{
   const formattedQuery = `SELECT b_id, business_name FROM business;`;
-  const businesses = executeQuery(formattedQuery);
-  res.send(businesses);
-  console.log("getBusinesses: ", businesses);
+  executeQuery(formattedQuery).then((businesses)=>{
+      console.log("getBusinesses: ", businesses[0]);
+      res.send(businesses[0]);
+  });
 
 });
 
 //this route returns the list of items associated to the business id.
 router.get('/getItems/:businessId',(req,res)=>{
 	const businessId = req.params.businessId;
-  const formattedQuery = `SELECT item_id, item_name, price FROM item WHERE b_id = ${businessId};`;
-  const items = executeQuery(formattedQuery);
-  res.send(items);
-  console.log("getItems: ", items);
+  const formattedQuery = `SELECT item_id, item_name, price FROM item WHERE b_id = "${businessId}";`;
+  executeQuery(formattedQuery).then((items)=>{
+    console.log("getItems: ", items[0]);
+    res.send(items[0]);
+  });
+  
+  
 
 });
 
 //this route gets the user's balance given the user's id.
 router.get('/getBal/:userId',(req,res)=>{
 	const userId = req.params.userId;
-  const formattedQuery = `SELECT balance FROM user WHERE u_id = ${userId};`;
-  const balanceResult = executeQuery(formattedQuery);
-  res.send(balanceResult);
-  console.log("getBal: ", balanceResult);
+  const formattedQuery = `SELECT balance FROM user WHERE u_id = "${userId}";`;
+  executeQuery(formattedQuery).then((balanceResult)=>{
+     res.send(balanceResult[0]);
+    console.log("getBal: ", balanceResult[0]);
+  });
+ 
 });
 
 //this route creates and inserts a new transaction, updates user balance, creates a new block record and returns the result of that query.
@@ -115,32 +100,35 @@ router.post('/newTransaction',(req,res)=>{
       transactionDateTime,
       newBalance} = req.body;
   
-  const formattedQuery = `INSERT INTO transaction(
-  item_id, u_id, b_id, transaction_hash, 
-  hash_version, block_id, transaction_date_time) 
-  values(
-    ${itemId}, 
-    ${userId}, 
-    ${businessId}, 
-    ${transactionHash},
-  ${hashVersion}, 
-  ${blockId}, 
-  ${transactionDateTime});`;
+  const formattedQuery = `INSERT INTO transaction(item_id, u_id, b_id, transaction_hash,hash_version, block_id,transaction_date_time) 
+  values("${itemId}", "${userId}", "${businessId}", "${transactionHash}","${hashVersion}", "${blockId}", "${transactionDateTime}");`;
+
+    const updateBalanceFormattedQuery =
+    `UPDATE user SET balance = "${newBalance}" WHERE u_id = "${userId}";
+    `;
+
+    const blockQuery = ``;
 
   //insert into transaction table
-  const insertResult = executeQuery(formattedQuery);
-  console.log("Inserting into transaction table: ",insertResult);
-  res.send(insertResult);
+  executeQuery(formattedQuery).then(insertResult =>{
+    console.log("Inserting into transaction table: ",insertResult);
 
-  //insert into block table
+    //update user balance
+    executeQuery(updateBalanceFormattedQuery).then(updateStatus=>{
+      console.log("updateBalance status: ",updateStatus);
 
+      //insert into block table
+      executeQuery(blockQuery).then(insertRes=>{
+        console.log("Inserting into block table: ", insertRes);
 
-  //update user balance
-  const updateBalanceFormattedQuery =
-  `UPDATE user SET balance = ${newBalance} WHERE u_id = ${userId};
-  `;
-  const updateStatus = executeQuery(updateBalanceFormattedQuery);
-  console.log("updateBalance status: ",updateStatus);
+        res.send({
+          "insertTransaction": insertResult,
+          "updateUserBalance": updateStatus,
+          "insertBlock": insertRes
+        });
 
+      });
+    });   
+  });
 });
 module.exports = router;
